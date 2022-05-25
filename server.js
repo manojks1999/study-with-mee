6e844711f44ade826b91e475a51d07a178d19ca0
manojks1999 <9743manoj@gmail.com>
2022-05-25 23:13:05 +0530

    make it better

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.custo
     params['ORDER_ID'] = 'TEST_'  + new Date().getTime();
     params['CUST_ID'] = paymentDetails.customerId;
     params['TXN_AMOUNT'] = paymentDetails.amount;
-    params['CALLBACK_URL'] = 'https://zooom-app-demo.herokuapp.com//callback';
+    params['CALLBACK_URL'] = 'https://zooom-app-demo.herokuapp.com/callback';
     params['EMAIL'] = paymentDetails.customerEmail;
     params['MOBILE_NO'] = paymentDetails.customerPhone;
  
